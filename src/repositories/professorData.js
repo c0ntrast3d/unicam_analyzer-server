5da6a1c807e1773f48109796afc339fd44c76fe3
Ryhor Pauliuts <ryhor.pauliuts@gmail.com>
2020-02-04 11:04:31 +0100

    - add: requireAuth middleware

--- src/repositories/professorData.js
+++ src/repositories/professorData.js
@@ -1,22 +1,15 @@
 const getResponse = require('../utils/getOrError')
 const procedures = require('../db/procedures')
-const getRegistrationNumber = require('../utils/idExtractor')
 
 const TYPES = require('tedious').TYPES
 
 const ProfessorDataRepository = (dbFunctions) => {
     const getProfessorData = (req, res) => {
 
-        const auth = req.headers['authorization']
         const { VarChar, Int } = TYPES
-        let number = 0
-        try {
-            number = getRegistrationNumber(auth)
-        } catch (error) {
-            return res.status(403).send({ error: error })
-        }
+
         const params = [
-            { name: 'MATR', type: VarChar, val: getRegistrationNumber(auth) },
+            { name: 'MATR', type: VarChar, val: req.userNumber },
             { name: 'DaAnnoP', type: Int, val: 2014 },
             { name: 'AAnnoP', type: Int, val: 2016 }
         ]
--- src/routes/createRouter.js
+++ src/routes/createRouter.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const requireAuth = require('../middleware/requireAuth')
 
 const createRouter = () => {
-  const router = express.Router()
-  require('./paths/professorData')(router)
-  require('./paths/sectionData')(router)
-  require('./paths/base')(router)
-  return router
+    const router = express.Router()
+    router.use(requireAuth)
+    require('./paths/professorData')(router)
+    require('./paths/sectionData')(router)
+    require('./paths/base')(router)
+    return router
 }
 
 module.exports = createRouter
